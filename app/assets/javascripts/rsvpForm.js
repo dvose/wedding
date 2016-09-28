25bc7887aaee4427af7e90a5a472740ee4678ee4
Dustin Vose <dustin@fivebox.com>
2016-09-28 15:15:07 -0400

    enabled overflow fix

--- app/assets/javascripts/rsvpForm.js
+++ app/assets/javascripts/rsvpForm.js
@@ -22,6 +22,9 @@ $(function() {
     console.log(data);
     console.log(status);
   }).bind('ajax:error', function(xhr, status, error) {
+  	$('.spinner').toggle();
+  	essage.className = 'alert alert-danger';
+    message.innerHTML = '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>' + 'Error occured when saving your RSVP. Please try again later.';
     console.log(error);
     console.log(status);
   });
